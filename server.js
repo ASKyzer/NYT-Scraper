d881e775fd2d3a2a099945c08caff3aad7e0d4a2
Adrian Kyzer <adriankyzer@gmail.com>
2018-04-05 03:56:23 -0400

    set up a GET request to scrape articles from NYT and sent as result object to our /scrape route.  Also did a GET request to retrievd the articles to send it back to the client.

--- server.js
+++ server.js
@@ -37,7 +37,57 @@ app.set("view engine", "handlebars");
 // By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+mongoose.connect("mongodb://localhost/scrapingthenews");
+
+// ROUTES //
+
+// Get request to scrape the NYT website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with request
+  request("https://www.nytimes.com/", function(error, response, html) {
+    // Then we load that into cheerio and save it to $ for a shorthand selector.
+    var $ = cheerio.load(html);
+    // Now, we grab every article tag and do the following:
+    $("article").each(function(i, element) {
+
+      // Save an empty result Object
+      var result = {};
+
+      // Add the title, summary and link and save them as properties of the result object
+      result.title = $(this).children("h2").text();
+      result.summary = $(this).children(".summary").text();
+      result.link = $(this).children("h2").children("a").attr("href");
+
+      // Create a new Article using the result object built from Scraping
+      db.Article.create(result)
+      .then(function(dbArticle) {
+        // View the added result in the console
+        console.log(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        return res.json(err);
+      })
+    }) // end of article.each function
+
+    // If we were able to sucessfully scrape and save the article, send a message to the client
+    res.send("Scrape Complete");
+  }) // end of request to the NYT
+}) // end of get request for /scrape route
+
+//* Route to get all the articles we scraped from mongodb
+app.get("/articles", function(req, res) {
+  // Grab everything from the Articles array
+  db.Article.find({})
+  .then(function(dbArticle) {
+    // If sucessful, send them back to the client
+    res.json(dbArticle);
+  })
+  .catch(function(err) {
+    // If an error occurred, send it to the client
+    res.json(err);
+  })
+}) //end of GET request to /articles route
 
 
 
