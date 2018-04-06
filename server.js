8ad019b90a3ff31415d492e72cbc410288d404f1
Adrian Kyzer <adriankyzer@gmail.com>
2018-04-06 04:37:08 -0400

    added heroku and mongo db connection

--- server.js
+++ server.js
@@ -36,9 +36,12 @@ app.set("view engine", "handlebars");
 
 // By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
 // Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/scrapingthenews");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapingthenews";
 
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
 // ROUTES //
 
 //GET requests to render Handlebars pages
